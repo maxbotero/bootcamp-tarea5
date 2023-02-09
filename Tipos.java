0046f9f6ca6b61ffa85e79c2fc398977a9221a6b
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-02-08 21:46:34 -0500

    Create Tipos.java

---
+++ Tipos.java
@@ -0,0 +1,30 @@
+package com.maxbotero;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+        //enteros
+        byte number1 = 1;
+        System.out.println(number1);
+        short number2 = 2;
+        System.out.println(number2);
+        int number3 = 3;
+        System.out.println(number3);
+        long number4 = 4;
+        System.out.println(number4);
+
+        //texto
+        String nombre = "Max";
+        System.out.println(nombre);
+
+        //decimales
+        double numDecimal = 2.5d;
+        System.out.println(numDecimal);
+        float numFloat = 1.6f;
+        System.out.println(numFloat);
+
+        //boolean
+        boolean registrado = false;
+        System.out.println(registrado);
+    }
+}
